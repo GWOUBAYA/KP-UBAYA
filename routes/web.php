3534e757dec9a9f7699310d6b4f643dfa00be0b3
Dendy Pratama <s160419122@student.ubaya.ac.id>
2022-05-30 23:28:35 +0700

    Update Status

--- routes/web.php
+++ routes/web.php
@@ -41,4 +41,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('questions_options_mass_destroy', ['uses' => 'QuestionsOptionsController@massDestroy', 'as' => 'questions_options.mass_destroy']);
     Route::resource('results', 'ResultsController');
     Route::post('results_mass_destroy', ['uses' => 'ResultsController@massDestroy', 'as' => 'results.mass_destroy']);
+    Route::put('/accept/{id}', 'ResultsController@updateAcc')->name("acc");
+    Route::put('/decline/{id}', 'ResultsController@updateDec')->name("dec");
+
 });
